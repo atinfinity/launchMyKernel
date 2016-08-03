84cf8b403c224a00e9d714e28c5d9d5124ad3585
atinfinity <dandelion1124@gmail.com>
2016-08-04 01:13:04 +0900

    fixed indent

--- launchMyKernel/Main.cpp
+++ launchMyKernel/Main.cpp
@@ -5,24 +5,24 @@
 
 int main(int argc, char *argv[])
 {
-	cv::Mat src = cv::imread("lena.jpg", cv::IMREAD_GRAYSCALE);
-	cv::Mat dst(src.size(), src.type(), cv::Scalar(0));
+    cv::Mat src = cv::imread("lena.jpg", cv::IMREAD_GRAYSCALE);
+    cv::Mat dst(src.size(), src.type(), cv::Scalar(0));
 
-	cv::cuda::GpuMat d_src(src);
-	cv::cuda::GpuMat d_dst(dst.size(), dst.type());
+    cv::cuda::GpuMat d_src(src);
+    cv::cuda::GpuMat d_dst(dst.size(), dst.type());
 
-	double f = 1000.0f/cv::getTickFrequency();
-	int64 start = 0, end = 0;
-	start = cv::getTickCount();
+    double f = 1000.0f / cv::getTickFrequency();
+    int64 start = 0, end = 0;
+    start = cv::getTickCount();
 
-	// 自作カーネルの呼び出し
-	launchMyKernel(d_src, d_dst);
+    // 自作カーネルの呼び出し
+    launchMyKernel(d_src, d_dst);
 
-	end = cv::getTickCount();
-	std::cout << ((end - start) * f) << " ms." << std::endl;
+    end = cv::getTickCount();
+    std::cout << ((end - start) * f) << " ms." << std::endl;
 
-	d_dst.download(dst);
-	cv::imwrite("dst.png", dst);
+    d_dst.download(dst);
+    cv::imwrite("dst.png", dst);
 
-	return 0;
+    return 0;
 }
